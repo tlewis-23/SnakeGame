7f8e12ccba527d5df6abca18acd62e91f90bfddb
Tyler Lewis <tyler.lewis[6603@gmail.com>
2023-04-16 02:59:00 -0400

    adjustments on how to move snake

--- src/main/java/com/techelevator/gamePanel.java
+++ src/main/java/com/techelevator/gamePanel.java
@@ -20,7 +20,7 @@ public class gamePanel extends JPanel implements ActionListener {
     int applesEaten;
     int appleX;
     int appleY;
-    int direction = 6;
+    char direction = 'R';
     boolean running = false;
     Timer timer;
     Random random;
@@ -90,16 +90,16 @@ public class gamePanel extends JPanel implements ActionListener {
         }
 
         switch (direction) {
-            case 8:
+            case 'U':
                 y[0] = y[0] - UNIT_SIZE;
                 break;
-            case 2:
+            case 'D':
                 y[0] = y[0] + UNIT_SIZE;
                 break;
-            case 4:
+            case 'L':
                 x[0] = x[0] - UNIT_SIZE;
                 break;
-            case 6:
+            case 'R':
                 x[0] = x[0] + UNIT_SIZE;
                 break;
 
@@ -171,24 +171,24 @@ public class gamePanel extends JPanel implements ActionListener {
         @Override
         public void keyPressed(KeyEvent e) {
             switch (e.getKeyCode()) {
-                case KeyEvent.VK_NUMPAD4:
-                    if (direction != 6) {
-                        direction = 4;
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R') {
+                        direction = 'L';
                     }
                     break;
-                case KeyEvent.VK_NUMPAD6:
-                    if (direction != 4) {
-                        direction = 6;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L') {
+                        direction = 'R';
                     }
                     break;
-                case KeyEvent.VK_NUMPAD8:
-                    if (direction != 2) {
-                        direction = 8;
+                case KeyEvent.VK_UP:
+                    if (direction != 'D') {
+                        direction = 'U';
                     }
                     break;
-                case KeyEvent.VK_NUMPAD2:
-                    if (direction != 8) {
-                        direction = 2;
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U') {
+                        direction = 'D';
                     }
                     break;
             }
